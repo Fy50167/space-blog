dffd09621d9bab63540dbfc149a3a266fe16e5fe
Mark Carlson <mcarlson@avenuecode.com>
2023-11-22 17:42:00 -0800

    Adds reducers.  Adds missing dependencies

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,44 @@
-import { Outlet } from 'react-router-dom';
+import { Outlet } from "react-router-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 
-export default function App () {  
+import { Provider } from "react-redux";
+import store from "./utils/store";
 
+const httpLink = createHttpLink({
+  uri: "/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("id_token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
+
+export default function App() {
   return (
-    <>
-      <main>
-        <Outlet />
-      </main>
-    </>
+    <ApolloProvider client={client}>
+      <div>
+        <Provider store={store}>
+          <main>
+            <Outlet />
+          </main>
+        </Provider>
+      </div>
+    </ApolloProvider>
   );
-};
-
+}
---
+++ client/src/utils/reducers.js
@@ -0,0 +1,16 @@
+const initialState = {
+  products: [],
+  categories: [],
+  currentCategory: "",
+  cart: [],
+  cartOpen: false,
+};
+
+export const reducers = (state = initialState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default reducers;
