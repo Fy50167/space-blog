e789b3ed969c0045e8fd1cb2dfa4b200e84fbde8
Tybalt Mall <tybaltmallet@Tybalts-MacBook-Air.local>
2023-11-27 08:05:36 -0800

    added shade on nav bar to show user what page they are currently on

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,11 +1,16 @@
 import React, { useState } from 'react';
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 
 export default function Navigation() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+    const location = useLocation();
 
     const toggleMobileMenu = () => {
         setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
+    const getNavLinkClass = (path) => {
+        return location.pathname === path ? 'nav-link-active bg-gray-400' : 'nav-link bg-[#f0f0f0]';
     }
 
     return (
@@ -15,8 +20,8 @@ export default function Navigation() {
                     <div className="text-lg font-bold text-gray-800">Space Blog</div>
                     
                     <div className={`md:flex ${isMobileMenuOpen ? "flex" : "hidden"} flex-col md:flex-row`}>
-                        <Link to="/" className="nav-link bg-[#f0f0f0] text-black rounded px-4 py-2 md:mr-4">Home</Link>
-                        <Link to="/profile" className="nav-link bg-[#f0f0f0] text-black rounded px-4 py-2 md:mr-4">Profile</Link>
+                        <Link to="/" className={`${getNavLinkClass('/')} text-black rounded px-4 py-2 md:mr-4`}>Home</Link>
+                        <Link to="/profile" className={`${getNavLinkClass('/profile')} text-black rounded px-4 py-2 md:mr-4`}>Profile</Link>
                         <Link to="/login" className="bg-green-500 text-white rounded px-4 py-2">Login</Link>
                     </div>
 
