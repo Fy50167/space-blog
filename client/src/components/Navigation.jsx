93b2ffe6b069d2c949e65372878c5cd086a66f45
Tybalt Mall <tybaltmallet@Tybalts-MacBook-Air.local>
2023-11-27 07:42:37 -0800

    added mobile responsiveness to nav bar

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,21 +1,28 @@
+import React, { useState } from 'react';
 import { Link } from "react-router-dom";
-import Auth from "../utils/auth"
-
 
 export default function Navigation() {
-    return(
+    const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setIsMobileMenuOpen(!isMobileMenuOpen);
+    }
+
+    return (
         <>
-            <nav className="navbar">
-                <div className="nav-content">
-                <div className="nav-logo">Space Blog</div>
-                
-                {/* Put the links here; one that says home and one that says profile. Profile route is / and nav route is /profile */}
-                <Link to="/" className="nav-link"> Home </Link>
-                <Link to="profile" className="nav-link">Profile</Link>
-                <Link to="/login" className="login-btn">
-                    Login
-                </Link>
-            
+            <nav className="fixed w-full bg-[#f0f0f0] shadow px-4 py-2 md:px-8">
+                <div className="flex justify-between items-center mx-auto max-w-[1000px]">
+                    <div className="text-lg font-bold text-gray-800">Space Blog</div>
+                    
+                    <div className={`md:flex ${isMobileMenuOpen ? "flex" : "hidden"} flex-col md:flex-row`}>
+                        <Link to="/" className="nav-link bg-[#f0f0f0] text-black rounded px-4 py-2 md:mr-4">Home</Link>
+                        <Link to="/profile" className="nav-link bg-[#f0f0f0] text-black rounded px-4 py-2 md:mr-4">Profile</Link>
+                        <Link to="/login" className="bg-green-500 text-white rounded px-4 py-2">Login</Link>
+                    </div>
+
+                    <button className="md:hidden" onClick={toggleMobileMenu}>
+                        Menu
+                    </button>
                 </div>
             </nav>
         </>
